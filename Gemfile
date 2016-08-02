77a2fd5b476973e0b002e6bfa91f40ae0f64f9a2
Phil Nash <philnash@gmail.com>
2016-08-02 11:25:07 +0100

    Updates code to use Twilio Add-ons instead of explicitly calling AlchemyAPI.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 ruby "2.3.1"
 gem 'sinatra', '>= 0'
 gem 'json', '>=0'
-gem 'alchemy-api-rb', :require => 'alchemy_api'
--- helloWorld.rb
+++ helloWorld.rb
@@ -1,23 +1,21 @@
 require 'sinatra'
 require 'json'
-require 'alchemy_api'
-
-def vcap_services
-  JSON.parse(ENV['VCAP_SERVICES'])
-end
-
-AlchemyAPI.key = vcap_services['user-provided'][1]['credentials']['apikey']
 
 post '/messages' do
   content_type "text/xml"
 
-  sentiment = AlchemyAPI.search(:sentiment_analysis, text: params["Body"])
+  add_ons = JSON.parse(params["AddOns"])
+  if add_ons["status"] == "successful"
+    sentiment = add_ons["results"]["ibm_watson_sentiment"]["result"]["docSentiment"]
 
-  case sentiment['type']
-  when "positive"
-    message = "Glad you're having a good day! Is there anything I can help with?"
-  when "negative"
-    message = "Sorry things aren't going so well, how can I help?"
+    case sentiment['type']
+    when "positive"
+      message = "Glad you're having a good day! Is there anything I can help with?"
+    when "negative"
+      message = "Sorry things aren't going so well, how can I help?"
+    else
+      message = "What can I help with?"
+    end
   else
     message = "What can I help with?"
   end
